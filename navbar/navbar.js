42bf1c9a9d1d8f0a66aef0a439232366d9e4e90f
Adnane Louardi <adnanelouardi-dev@gmail.com>
2021-04-11 08:53:10 +0100

    sidebare

--- navbar/navbar.js
+++ navbar/navbar.js
@@ -1,18 +1,32 @@
-let humburger = document.getElementById("humburger"),
-   navContainer = document.getElementById("navContainer"),
-   chevron = document.getElementById("chevron"),
-   iconTitle = document.getElementById("iconTitle");
+var navContainer = document.getElementById("navContainer"),
+   chevron = document.getElementById("chevron");
 
-const sideBareEffect = () => {
-   if (navContainer.style.width == "5%") {
-      navContainer.style.width = "15%";
-
-      chevron.classList.remove("fa-chevron-right");
-      chevron.classList.add("fa-chevron-left");
-   } else {
-      navContainer.style.width = "5%";
+chevron.onclick = () => {
+   if (navContainer.style.width === "150px") {
+      navContainer.style.width = "55px";
 
       chevron.classList.remove("fa-chevron-left");
+
       chevron.classList.add("fa-chevron-right");
+   } else {
+      navContainer.style.width = "150px";
+
+      chevron.classList.remove("fa-chevron-right");
+
+      chevron.classList.add("fa-chevron-left");
    }
 };
+// const onLoad = () => {
+//    let iconTitle = document.getElementsByClassName("iconTitle");
+//    if (navContainer.style.width === "55px") {
+//       let iconTitle = document.getElementsByClassName("iconTitle");
+//       for (let i = 0; i < iconTitle.length; i++) {
+//          iconTitle[i].style.display = "none";
+//       }
+//    } else {
+//       for (let i = 0; i < iconTitle.length; i++) {
+//          let iconTitle = document.getElementsByClassName("iconTitle");
+//          iconTitle[i].style.display = "inline";
+//       }
+//    }
+// };
